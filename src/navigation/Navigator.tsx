cbf765d29e7cae46110ad0489acd4964925c105e
AlfredoGV77 <jalfredogtv@gmail.com>
2022-05-10 19:38:09 -0500

    Se agrego el Detalle

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -9,8 +9,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../Screens/LoginScreen';
 import RegisterScreen from '../Screens/RegisterScreen';
 import { Plantas } from '../Screens/Plantas';
+import { DetallePlanta } from '../Screens/DetallePlanta';
+import { SimplePlanta } from '../interfaces/plantaInterfaces';
 
-const Stack = createStackNavigator();
+
+export type RootStackParams={
+  Plantas: undefined;
+  DetallePlanta: {simplePlanta:SimplePlanta, color:string};
+}
+const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigator = () => {
   return (
@@ -23,6 +30,7 @@ export const Navigator = () => {
       {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
       {/* <Stack.Screen name="RegisterScreen" component={RegisterScreen}/> */}
       <Stack.Screen name="Plantas" component={Plantas}/>
+      <Stack.Screen name="DetallePlanta" component={DetallePlanta}/>
 
     </Stack.Navigator>
   );
