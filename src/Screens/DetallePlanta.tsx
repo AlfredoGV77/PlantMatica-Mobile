6264cb7f00709f9ec72baaa38ae5ab8ccd7084b6
AlfredoGV77 <jalfredogtv@gmail.com>
2022-05-10 19:37:30 -0500

    Creacion del Archivo

---
+++ src/Screens/DetallePlanta.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+export const DetallePlanta = () => {
+  return (
+    <div>DetallePlanta</div>
+  )
+}
