0ba59db8fbe30e44a26d8a696e653bf0ce01e957
AlfredoGV77 <jalfredogtv@gmail.com>
2022-05-14 15:02:24 -0500

    Ya se ve bonito:D

--- src/Styles/DetallePlantaStyle.tsx
+++ src/Styles/DetallePlantaStyle.tsx
@@ -12,12 +12,25 @@ import {StyleSheet,Dimensions } from "react-native";
 export const DetallePlantaStyle = StyleSheet.create({
     Header:{
         zIndex:999,
-        height:Dimensions.get('window').height / 3,
-        alignItems:'center',
+        height:Dimensions.get('window').height / 2.5,
         borderBottomRightRadius:1000,
         borderBottomLeftRadius:1000,
     },
     BackButton:{
+        left:15,
+    },
+    Title:{
+        fontSize:40,
+        textAlignVertical: "center",
+        textAlign: "center",
+        color:'white',
+        fontFamily: 'Sans-serif-Roboto',
+    },
+    Imagen:{
+        top:-120,
+        width:150,
+        height:150,
+        left:120,
     },
 })
 
