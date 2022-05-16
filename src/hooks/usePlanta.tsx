164bb9afe8e574bfcbefd89b9b67e25d05a568e8
AlfredoGV77 <jalfredogtv@gmail.com>
2022-05-16 01:53:47 -0500

    Cargar Info COMPLETA

--- src/hooks/usePlanta.tsx
+++ src/hooks/usePlanta.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable prettier/prettier */
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import { useState, useEffect } from 'react';
+import { PlantaFullInformation } from '../interfaces/plantaInterfaces';
+import { plantaApi } from '../api/plantaApi';
 
-export const usePlanta = () => {
-  return (
-    <div>usePlanta</div>
-  )
-}
+export const usePlanta = (_id:string) => {
+  const [isLoading,setIsLoading] = useState(true)
+  const [Planta,setPlanta] = useState<PlantaFullInformation>()
+
+  const loadPlanta = async() => {
+      const resp = await plantaApi.get<PlantaFullInformation>(`https://plantmatica-api.vercel.app/ficha/${ _id }`);
+      setPlanta(resp.data);
+      setIsLoading(false);
+  }
+
+  useEffect(()=>{
+    loadPlanta();
+  },[])
+
+  return {
+      isLoading,
+      Planta,
+  }
+
+};
