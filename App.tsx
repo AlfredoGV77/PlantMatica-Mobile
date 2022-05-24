2c9e792928c92c024ec279be03cf015bfa28864a
AlfredoGV77 <jalfredogtv@gmail.com>
2022-05-24 06:08:35 -0500

    Agregamos el NavigatorMain como unico elemento

--- App.tsx
+++ App.tsx
@@ -1,16 +1,31 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable eol-last */
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
 // import {Navigator} from './src/navigation/Navigator';
 import { Tabs } from './src/navigation/Tabs';
+import { NavigatorMain } from './src/navigation/NavigatorMain';
+import { AuthProvider } from './src/context/AuthContext';
 
+
+const AppState = ({children}:{children:JSX.Element | JSX.Element[]})=>{
+    return (
+        <AuthProvider>
+            {children}
+        </AuthProvider>
+    );
+};
 
 export const App = () => {
    return (
       <NavigationContainer>
-          {/* <Navigator/> */}
-          <Tabs/>
+          {/* <Tabs/> */}
+          {/* <TabLogin/> */}
+          <AppState>
+            <NavigatorMain/>
+          </AppState> 
       </NavigationContainer>
   );
 };
