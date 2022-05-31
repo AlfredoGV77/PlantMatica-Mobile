ed44f82b8ba894b14623e5119031270a411b5d56
AlfredoGV77 <jalfredogtv@gmail.com>
2022-05-31 12:42:19 -0500

    Cosillas por ahi

--- src/api/plantaApi.tsx
+++ src/api/plantaApi.tsx
@@ -1,19 +1,5 @@
 /* eslint-disable prettier/prettier */
 import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const baseURL = 'https://mmg7n2ixnk.us-east-2.awsapprunner.com';
-export const plantaApi = axios.create({baseURL});
-
-
-plantaApi.interceptors.request.use(
-    async(config)=>{
-        const token = await AsyncStorage.getItem('token');
-        if (token){
-            config.headers!['x-token'] = token;
-        }
-        return config;
-    }
-);
+export const plantaApi = axios.create();
 
 export default plantaApi;
